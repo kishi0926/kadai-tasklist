4c9b21a6510d5703385e8ab588917a39423b285e
risa.kishi <ec2-user@ip-172-31-94-4.ec2.internal>
2020-10-23 10:52:22 +0000

    修正追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(id: :desc).page(params[:page])
-    counts(@user)
   end
 
   def new
